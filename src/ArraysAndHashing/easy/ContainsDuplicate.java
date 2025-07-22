94c8000779dbc2ea15d803be3fb7c96f0e4925ae
Nandhini Kesavan <nandhinikesavan011@gmail.com>
2025-07-22 21:34:22 +0530

    refactor: reorganize file structure under ArraysAndHashing

---
+++ src/ArraysAndHashing/easy/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package ArraysAndHashing.easy;
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i: nums){
+            if(set.contains(i)){
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
---
+++ src/ArraysAndHashing/easy/ValidAnagram.java
@@ -0,0 +1,30 @@
+package ArraysAndHashing.easy;
+
+import java.util.HashMap;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s,t));
+    }
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);   // a-3, n-1, g-1, r-1, m-1
+        }
+
+        for(char c : t.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)-1);
+            if(map.get(c)< 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}

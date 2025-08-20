b6cebd89ed0d6ca0199eaa31081c3f12b448ad9f
Nandhini Kesavan <nandhinikesavan011@gmail.com>
2025-08-20 10:35:17 +0530

    chore: added solution for stack problem

---
+++ src/Stacks/RemoveStarsInString.java
@@ -0,0 +1,27 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class RemoveStarsInString {
+    public static void main(String[] args) {
+        String s = "leet**cod*e";
+        System.out.println(removeStars(s));
+    }
+    public static String removeStars(String s){
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char val = s.charAt(i);
+            if(val == '*'){
+                st.pop();
+            }
+            else{
+                st.push(val);
+            }
+        }
+        String res  = "";
+        for(Character i : st){
+            res+=i;
+        }
+        return res;
+    }
+}
